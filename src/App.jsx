6161e3d445bde44431d05a8b83bf14a06bb1ad44
mali041 <ali041gohar@gmail.com>
2024-03-17 11:37:07 +0500

    setting-up-appwrite-config

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import "./App.css";
+import conf from "./conf";
 
 function App() {
+  console.log(conf.appwriteBucketId);
+  console.log(conf.appwriteCollectionId);
+  console.log(conf.appwriteDatabaseId);
+  console.log(conf.appwriteProjectId);
+
   return (
     <>
       <h1> Blog App with Appwrite</h1>
