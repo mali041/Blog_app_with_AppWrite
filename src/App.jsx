b915ba0d4e08fea9e77ac22d16b7e9faebf08a9a
mali041 <ali041gohar@gmail.com>
2024-03-21 10:42:32 +0500

    adding-tailwind-&-some-comp-learning-forwardRef(Hook)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/features/authSlice";
+import { Footer, Header } from "./components";
 import conf from "./conf";
 
 function App() {
-  console.log(conf.appwriteUrl);
-  return (
-    <>
-      <h1> Blog App with Appwrite</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  console.log(conf.appwriteBucketId);
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-slate-500">
+      <div className="w-full block">
+        <Header />
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Container from "./container";
+import LogoutBtn from "./Header/LogoutBtn";
+import Logo from "./Logo";
+import Button from "./Button";
+import Input from "./Input";
+export { Header, Footer, Container, LogoutBtn, Logo, Input, Button };
